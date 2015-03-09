2afa857e52c3455cfed3e7e9659a8dc96cec35f0
DerekRivers <dyk3rz@googlemail.com>
2015-03-09 09:07:47 +0000

    MVC

---
+++ Views/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Views.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+
+            ViewBag.Message = "Hello, World";
+            ViewBag.Time = DateTime.Now.ToShortDateString();
+
+            return View("DebugData");
+        }
+
+        public ActionResult List()
+        {
+            return View();
+        }
+    }
+}
